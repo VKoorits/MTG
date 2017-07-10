47206f3c7517fbdb42b1e1fe7094f0faa66da710
VKoorits <koorits.viktor@yandex.ru>
2017-07-11 00:58:31 +0300

    no dublicate

--- lib/MTGapp.pm
+++ lib/MTGapp.pm
@@ -10,7 +10,7 @@ use Parser 'parse';
 use DDP;
 
 our $VERSION = '0.1';
-my $ONE_CARD_PRICE = 100;
+my $ONE_CARD_PRICE = 0;
 
 
 my $db_handler = undef;
@@ -59,14 +59,27 @@ sub deal {
 		
 		$card_id = $dbh->selectall_arrayref($id_stmt);
 	}
-	
-	$dbh->do("INSERT INTO available_cards (user_id, card_id) VALUES (".session('user_id').",".$card_id->[0]->[0].") ");
-	$dbh->do( "UPDATE users SET balance="
-			  .(session('balance')-$ONE_CARD_PRICE)
-			  ." WHERE user_id="
-			  .session('user_id')
-			);
-	session balance => session('balance')-$ONE_CARD_PRICE;
+	#проверка: у пользователя уже может быть эта карта
+	my $has_card = $dbh->selectall_arrayref(
+		"SELECT COUNT(*) FROM available_cards "
+		."WHERE user_id="
+		.session('user_id')
+		." and card_id="
+		.$card_id->[0]->[0]
+	);
+	print "\n\ncount_this_card => ".$has_card->[0]->[0]."\n\n";
+	if( $has_card->[0]->[0] <= 0) {	
+		$dbh->do("INSERT INTO available_cards (user_id, card_id) VALUES (".session('user_id').",".$card_id->[0]->[0].") ");
+		$dbh->do( "UPDATE users SET balance="
+				  .(session('balance')-$ONE_CARD_PRICE)
+				  ." WHERE user_id="
+				  .session('user_id')
+				);
+		session balance => session('balance')-$ONE_CARD_PRICE;
+		redirect '/';
+	} else {
+		template 'get_card', {csrf_token => get_csrf_token(), ok => 1, message => "У вас уже есть такакя карта. Попробуйте ещё раз"};
+	}
 	
 }
 
@@ -74,8 +87,8 @@ sub deal {
 
 hook 'before' => sub {
 		set session => 'simple';
-		session user_id => 1;
-		session balance => 1000;
+		#session user_id => 1;
+		#session balance => 1000;
 		if (request->path_info !~ m{^/login} &&
 			request->path_info !~ m{^/CSRF} &&
 		 	!session('user_id')) {
@@ -86,9 +99,7 @@ hook 'before' => sub {
 			if ( !$csrf_token || !validate_csrf_token($csrf_token) ) {
 				redirect '/CSRF';
 			}
-		}
-
-		
+		}		
     };
 
 
@@ -112,19 +123,33 @@ END
 post '/get_card' => sub {
 	my $query = params->{query};
 	my @cards = @{ get_magic_cards($query) };
-	#TODO! занесение в базу
+	
+	my $dbh = connect_db();
+	for my $card (@cards) {
+		my $id_stmt = "SELECT card_id FROM pictures WHERE card_name=\"$card->{card_name}\"";
+		p $id_stmt;
+		my $card_id = $dbh->selectall_arrayref($id_stmt);
+		if(not defined $card_id->[0]) {
+			my $stmt = "INSERT INTO pictures (card_name, link) VALUES (\"$card->{card_name}\", \"$card->{link}\")";
+			$dbh->do($stmt);
+		}
+	}
+	
 	
 	if( @cards == 0) {
-		template 'get_card', {message => "По вашему запросу не найдено карт :( попробуйте ещё раз"}
+		template 'get_card', {csrf_token => get_csrf_token(), ok => 1, message => "По вашему запросу не найдено карт :( попробуйте ещё раз"}
 	} elsif( @cards == 1 ) {
 		deal($cards[0]);
-		redirect '/';
 	} else {
 		print "\nbuy_card\n";
-		template 'select_card', { cards => \@cards, csrf_token => get_csrf_token() };
+		template 'select_card', { cards => \@cards, csrf_token => get_csrf_token()};
 	}
 };
-any ["get", "head"] => '/get_card' => sub { template 'get_card', { csrf_token => get_csrf_token() } };
+any ["get", "head"] => '/get_card' => sub {
+	my $ok = 1;
+	$ok = 0 if( session('balance') < $ONE_CARD_PRICE );
+	print "\nok => $ok\n";
+	template 'get_card', { csrf_token => get_csrf_token(), ok => $ok, message =>"" } };
 
 
 post '/proof' => sub {
